cd10c722a6c09b74b36fc777e524e9ad5c028028
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2019-08-27 14:15:41 -0500

    Fix conflicting css

--- js/app/pivotal-tracker/member-filter.js
+++ js/app/pivotal-tracker/member-filter.js
@@ -7,8 +7,8 @@ function setMemberEventHandler(){
             "let search_field = $(\"input[name='search']\");" +
             "search_field.val(\"mywork:\\\"\"+initials+\"\\\"\");" +
             "search_field.focus().submit();" +
-            "$('.dropdown-content').hide();" +
-            "setTimeout(function(){$('.dropdown-content').attr('style','');},500);" +
+            "$('.ris-pv-dropdown-content').hide();" +
+            "setTimeout(function(){$('.ris-pv-dropdown-content').attr('style','');},500);" +
         "});";
     (document.head || document.documentElement).appendChild(memberEventHandler);
 }
@@ -27,7 +27,7 @@ function createMembersDropDown(members) {
     $($('header>ul')[0]).append("<li class='tc_pull_right'>" +
         "<div class='dropdown'>" +
             "<button class='SMkCk__Button _3jN8d__Button--header Dropdown__button'>Member Filters</button>" +
-            "<div class='dropdown-content'>" +
+            "<div class='ris-pv-dropdown-content'>" +
                 members_elems.join('') +
             "</div>" +
         "</div>");
